9dc73bf3b475a035947bd3e7f67531ffb14371f8
Thibault Bondetti <thibault.bondetti@cgi.com>
2026-08-12 08:45:43 +0200

    [init] Spring initializr

---
+++ src/main/java/io/github/tbondetti/testoauth2client/TestOauth2ClientApplication.java
@@ -0,0 +1,13 @@
+package io.github.tbondetti.testoauth2client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOauth2ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestOauth2ClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/tbondetti/testoauth2client/TestOauth2ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tbondetti.testoauth2client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestOauth2ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
