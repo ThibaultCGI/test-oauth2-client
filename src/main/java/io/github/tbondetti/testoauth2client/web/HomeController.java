a50d22f317f92dcca22636235c50097aa89ac0f3
ThibaultCGI <thibault.bondetti@cgi.com>
2026-08-17 09:58:27 +0200

    [feat] OIDC + configuration spring security (#1)

---
+++ src/main/java/io/github/tbondetti/testoauth2client/web/HomeController.java
@@ -0,0 +1,19 @@
+package io.github.tbondetti.testoauth2client.web;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public Map<String, Object> home(
+            @AuthenticationPrincipal final OidcUser user
+    ) {
+        return user.getClaims();
+    }
+}
